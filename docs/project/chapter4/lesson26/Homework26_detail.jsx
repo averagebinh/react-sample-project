272fd0ed721c2ecab5aeb1d674093aa96fdddc03
Nguyen Thanh Binh <averagebinh@gmail.com>
2024-11-28 09:41:56 +0700

    "#26 display data"

--- docs/project/chapter4/lesson26/Homework26_detail.jsx
+++ docs/project/chapter4/lesson26/Homework26_detail.jsx
@@ -1,10 +1,29 @@
 import { useEffect, useState } from 'react';
 import { getUserWithPaginate } from '../../utils/apiService';
-
+import User from './User';
+import './Homeword26_detail.scss';
 //   Ở đây, truyền vào mặc định là getUserWithPaginate (1, 3) để có dữ liệu
 
 const Homework26 = (props) => {
-  return <div></div>;
+  const [listUsers, setListUsers] = useState([]);
+  useEffect(() => {
+    fetchListUSer();
+  }, []);
+
+  const fetchListUSer = async () => {
+    let res = await getUserWithPaginate(1, 3);
+    console.log(res);
+    setListUsers(res.DT.users);
+  };
+  console.log('>>>listUsers ', listUsers);
+  return (
+    <div className='list-user-container'>
+      {listUsers &&
+        listUsers.map((item) => {
+          return <User data={item} />;
+        })}
+    </div>
+  );
 };
 
 export default Homework26;
