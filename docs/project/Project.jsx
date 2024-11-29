d41b496561b9006129a65861235ec70f95261935
Nguyen Thanh Binh <averagebinh@gmail.com>
2024-11-29 15:38:02 +0700

    "#mutate state of objects in array using immer"

--- docs/project/Project.jsx
+++ docs/project/Project.jsx
@@ -1,14 +1,14 @@
+import Lesson36 from './chapter4/lesson36/Lesson36';
 import Login from './Login';
 import './Project.scss';
 import 'nprogress/nprogress.css';
 // import Lesson26 from './chapter4/lesson26/Lesson26';
-import Lesson35 from './chapter4/lesson35/Lesson35';
 
 const Project = (props) => {
   return (
     <div className='project'>
       <Login />
-      <Lesson35 />
+      <Lesson36 />
     </div>
   );
 };
